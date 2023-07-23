fb5b09d99b45e8f17d72240d7ff0e1e3964b5db9
Gabriel Dias <117274518+DaysGabriel@users.noreply.github.com>
2023-07-23 16:04:31 -0300

    Update intenger_float_division.cpp

--- intenger_float_division.cpp
+++ intenger_float_division.cpp
@@ -1,17 +1,20 @@
 /*Objective : train the standard header for competitive programming*/
 /*Intenger and float point division*/
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-		ios::sync_with_stdio(0);
-		cin.tie(0);
-	
-    float a, b;
-    cin >> a >> b;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	float a, b;
+
+	cin >> a >> b;
     
-    cout << (int) (a/b) << 
-    "\n" << (int) a % (int) b << "\n" <<
-    setprecision(2) << fixed << a/b << "\n"; 
+	cout << (int) (a/b) << 
+	"\n" << (int) a % (int) b << "\n" <<
+	setprecision(2) << fixed << a/b << "\n"; 
 }
