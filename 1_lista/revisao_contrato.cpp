f4b5a2d25b05fe4f0faf7bcf458684f1319408d8
DaysGabriel <gabriel.dias0404@gmail.com>
2023-07-28 19:53:16 -0300

    versao certa II

--- 1_lista/revisao_contrato.cpp
+++ 1_lista/revisao_contrato.cpp
@@ -1,6 +1,7 @@
 /*https://chat.openai.com/share/b9ce2b10-373d-4ac6-9a5e-2b11a7024842
 https://chat.openai.com/share/d674713a-25ac-40a9-98be-2b38735dacaf
-https://chat.openai.com/share/b9ce2b10-373d-4ac6-9a5e-2b11a7024842*/
+https://chat.openai.com/share/b9ce2b10-373d-4ac6-9a5e-2b11a7024842
+https://chat.openai.com/share/f58d29f2-83ff-4254-afa6-32c1b5d8e969*/
 #include <bits/stdc++.h>
 using namespace std;
 
